cc1345d4cc815d464e91e6d00f220502190dba9b
3oraby <mohaahmed21102003@gmail.com>
2024-08-17 02:09:50 +0300

    .

--- lib/features/home/constants/home_page_constants.dart
+++ lib/features/home/constants/home_page_constants.dart
@@ -11,6 +11,7 @@ class HomePageConstants {
   static const Color selectedColor = Colors.black;
   static const Color unSelectedColor = Colors.grey;
 
+
   // home tab body
   static const List homeTapBodySalePhotos = [
     AppImages.salesPhoto1,
