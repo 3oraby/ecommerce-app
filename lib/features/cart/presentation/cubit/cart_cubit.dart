d2810e41e1a2e8701078546bd6163603c4ac5bca
3oraby <mohaahmed21102003@gmail.com>
2024-08-28 17:44:32 +0300

    add cart cubit

--- lib/features/cart/presentation/cubit/cart_cubit.dart
+++ lib/features/cart/presentation/cubit/cart_cubit.dart
@@ -1,8 +1,101 @@
-
-import 'package:e_commerce_app/features/cart/presentation/cubit/cart_state.dart';
+import 'package:e_commerce_app/features/cart/data/models/add_to_cart_response_model.dart';
+import 'package:e_commerce_app/features/cart/data/models/delete_from_cart_response_model.dart';
+import 'package:e_commerce_app/features/cart/data/models/show_cart_response_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'cart_state.dart';
+import 'package:e_commerce_app/features/cart/data/repositories/cart_repository.dart';
 
 class CartCubit extends Cubit<CartState> {
-  CartCubit() : super(CartInitial());
+  final CartRepository cartRepository;
+
+  CartCubit({required this.cartRepository}) : super(CartInitialState());
+
+  Future<void> showCart() async {
+    emit(ShowCartLoadingState());
+    try {
+      final ShowCartResponseModel cart = await cartRepository.showCart();
+      if (cart.cartItems!.isEmpty) {
+        emit(EmptyCartState());
+      } else {
+        emit(ShowCartLoadedState(cart: cart));
+      }
+    } catch (e) {
+      emit(ShowCartErrorState(message: 'Failed to load cart: $e'));
+    }
+  }
+
+  Future<void> addItemToCart(int productId) async {
+    emit(AddToCartLoadingState());
+    try {
+      final AddToCartResponseModel addToCartResponseModel =
+          await cartRepository.addToCart(productId);
+      if (addToCartResponseModel.status) {
+        await showCart();
+      } else {
+        emit(const AddToCartErrorState(message: 'Failed to add item to cart'));
+      }
+    } catch (e) {
+      emit(AddToCartErrorState(message: 'Failed to add item to cart: $e'));
+    }
+  }
+
+  Future<void> deleteItemFromCart(int cartItemId) async {
+    emit(DeleteFromCartLoadingState());
+    try {
+      final DeleteFromCartResponseModel deleteFromCartResponseModel =
+          await cartRepository.deleteFromCart(cartItemId);
+      if (deleteFromCartResponseModel.status) {
+        await showCart();
+      } else {
+        emit(const DeleteFromCartErrorState(
+            message: 'Failed to delete item from cart'));
+      }
+    } catch (e) {
+      emit(DeleteFromCartErrorState(
+          message: 'Failed to delete item from cart: $e'));
+    }
+  }
+
+  Future<void> updateCartItem(
+      {required int cartId, required int quantity}) async {
+    emit(CartLoadingState());
+    try {
+      final bool success = await cartRepository.updateCartItem(
+          cartId: cartId, newQuantity: quantity);
+      emit(CartItemUpdatedState(success: success));
+      if (success) {
+        await showCart();
+      } else {
+        emit(const CartErrorState(message: 'Failed to update item quantity'));
+      }
+    } catch (e) {
+      emit(CartErrorState(message: 'Failed to update item quantity: $e'));
+    }
+  }
+
+  Future<void> loadCartPrice() async {
+    emit(CartLoadingState());
+    try {
+      final String price = await cartRepository.showCartPrice();
+      emit(CartPriceLoadedState(price: price));
+    } catch (e) {
+      emit(CartErrorState(message: 'Failed to load cart price: $e'));
+    }
+  }
+
+  Future<void> showCartAndPrice() async {
+    emit(ShowCartLoadingState());
+    try {
+      final ShowCartResponseModel cart = await cartRepository.showCart();
+      final String price = await cartRepository.showCartPrice();
+
+      if (cart.cartItems!.isEmpty) {
+        emit(EmptyCartState());
+      } else {
+        emit(CartAndPriceLoadedState(cart: cart, price: price));
+      }
+    } catch (e) {
+      emit(ShowCartErrorState(message: 'Failed to load cart: $e'));
+    }
+  }
 }
---
+++ lib/features/cart/presentation/cubit/cart_state.dart
@@ -0,0 +1,66 @@
+import 'package:e_commerce_app/features/cart/data/models/show_cart_response_model.dart';
+
+abstract class CartState {
+  const CartState();
+}
+
+// Initial state
+class CartInitialState extends CartState {}
+
+// General Loading State (if needed)
+class CartLoadingState extends CartState {}
+
+// Loading and error states for adding to cart
+class AddToCartLoadingState extends CartState {}
+
+class AddToCartErrorState extends CartState {
+  final String message;
+  const AddToCartErrorState({required this.message});
+}
+
+// Loading and error states for deleting from cart
+class DeleteFromCartLoadingState extends CartState {}
+
+class DeleteFromCartErrorState extends CartState {
+  final String message;
+  const DeleteFromCartErrorState({required this.message});
+}
+
+// Loading and error states for showing the cart
+class ShowCartLoadingState extends CartState {}
+
+class ShowCartErrorState extends CartState {
+  final String message;
+  const ShowCartErrorState({required this.message});
+}
+
+class EmptyCartState extends CartState{}
+
+class CartAndPriceLoadedState extends CartState{
+  final String price;
+  final ShowCartResponseModel cart;
+
+  CartAndPriceLoadedState({required this.price, required this.cart});
+
+}
+
+// States for successful operations
+class CartErrorState extends CartState {
+  final String message;
+  const CartErrorState({required this.message});
+}
+
+class ShowCartLoadedState extends CartState {
+  final ShowCartResponseModel cart;
+  const ShowCartLoadedState({required this.cart});
+}
+
+class CartItemUpdatedState extends CartState {
+  final bool success;
+  const CartItemUpdatedState({required this.success});
+}
+
+class CartPriceLoadedState extends CartState {
+  final String price;
+  const CartPriceLoadedState({required this.price});
+}
