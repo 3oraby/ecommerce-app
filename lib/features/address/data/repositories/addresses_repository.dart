3295405270c4326c73ec1a7fb90456bf2ff91569
3oraby <mohaahmed21102003@gmail.com>
2024-09-01 16:47:12 +0300

    add orders addresses repository

--- lib/features/address/data/repositories/addresses_repository.dart
+++ lib/features/address/data/repositories/addresses_repository.dart
@@ -1,5 +1,8 @@
 import 'package:e_commerce_app/features/address/data/models/get_all_addresses_response_model.dart';
+import 'package:e_commerce_app/features/address/data/models/get_orders_addresses_response_model.dart';
 
 abstract class AddressesRepository {
   Future<GetAllAddressesResponseModel> getAllAddresses();
+
+  Future<GetOrdersAddressesResponseModel> getOrdersAddresses();
 }
--- lib/features/address/data/repositories/addresses_repository_impl.dart
+++ lib/features/address/data/repositories/addresses_repository_impl.dart
@@ -1,13 +1,24 @@
 import 'package:e_commerce_app/features/address/data/data_sources/get_all_addresses_service.dart';
+import 'package:e_commerce_app/features/address/data/data_sources/get_all_orders_addresses_service.dart';
 import 'package:e_commerce_app/features/address/data/models/get_all_addresses_response_model.dart';
+import 'package:e_commerce_app/features/address/data/models/get_orders_addresses_response_model.dart';
 import 'package:e_commerce_app/features/address/data/repositories/addresses_repository.dart';
 
 class AddressesRepositoryImpl implements AddressesRepository {
   final GetAllAddressesService getAllAddressesService;
-  AddressesRepositoryImpl({required this.getAllAddressesService});
+  final GetAllOrdersAddressesService getAllOrdersAddressesService;
+  AddressesRepositoryImpl({
+    required this.getAllAddressesService,
+    required this.getAllOrdersAddressesService,
+  });
 
   @override
   Future<GetAllAddressesResponseModel> getAllAddresses() async {
     return await getAllAddressesService.getAllAddresses();
+  }
+
+  @override
+  Future<GetOrdersAddressesResponseModel> getOrdersAddresses() async {
+    return await getAllOrdersAddressesService.getOrdersAddresses();
   }
 }
