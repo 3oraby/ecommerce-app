2b70e543838fd24a39d2d5e80c753649e48a029b
3oraby <mohaahmed21102003@gmail.com>
2024-09-05 16:47:36 +0300

    make product repository

---
+++ lib/features/products/data/repositories/product_repository.dart
@@ -0,0 +1,11 @@
+import 'package:e_commerce_app/features/products/data/models/get_home_details_model.dart';
+import 'package:e_commerce_app/features/products/data/models/get_products_response_model.dart';
+
+abstract class ProductRepository {
+  Future<GetHomeDetailsResponseModel> getHomeData();
+
+  Future<GetProductsCategoryResponseModel> getProductsByCategory({
+    required int categoryId,
+  });
+
+}
---
+++ lib/features/products/data/repositories/product_repository_impl.dart
@@ -0,0 +1,26 @@
+import 'package:e_commerce_app/features/products/data/data_sources/get_home_details_service.dart';
+import 'package:e_commerce_app/features/products/data/data_sources/get_product_by_category_service.dart';
+import 'package:e_commerce_app/features/products/data/models/get_home_details_model.dart';
+import 'package:e_commerce_app/features/products/data/models/get_products_response_model.dart';
+import 'package:e_commerce_app/features/products/data/repositories/product_repository.dart';
+
+class ProductRepositoryImpl implements ProductRepository {
+  final GetHomeDetailsService getHomeDetailsService;
+  final GetProductByCategoryService getProductByCategoryService;
+
+  ProductRepositoryImpl({
+    required this.getHomeDetailsService,
+    required this.getProductByCategoryService,
+  });
+
+  @override
+  Future<GetHomeDetailsResponseModel> getHomeData() async{
+    return await getHomeDetailsService.getHomeData();
+  }
+
+  @override
+  Future<GetProductsCategoryResponseModel> getProductsByCategory(
+      {required int categoryId}) async {
+    return await getProductsByCategory(categoryId: categoryId);
+  }
+}
