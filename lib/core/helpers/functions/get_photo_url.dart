2a6b64460e7c2ac46de4745938252f3b5b6ee2d5
3oraby <mohaahmed21102003@gmail.com>
2024-09-08 23:49:25 +0300

    add method to get photo url

---
+++ lib/core/helpers/functions/get_photo_url.dart
@@ -0,0 +1,5 @@
+import 'package:e_commerce_app/constants/api_constants.dart';
+
+String getPhotoUrl(String photoPath) {
+  return '${ApiConstants.baseUrl}${ApiConstants.getPhotoEndPoint}$photoPath';
+}
