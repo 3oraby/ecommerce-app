f5527c25573c3c756fba7e6da0887c16720e007f
3oraby <mohaahmed21102003@gmail.com>
2024-09-25 03:01:20 +0300

    add more images for aplication

--- lib/core/utils/app_assets/images/app_images.dart
+++ lib/core/utils/app_assets/images/app_images.dart
@@ -6,13 +6,29 @@ class AppImages {
   /// assets/images/app_logo.png
   static const String imagesAppLogo = "assets/images/app_logo.png";
 
+  /// Assets for imagesEmptyCart
+  /// assets/images/empty_cart.png
+  static const String imagesEmptyCart = "assets/images/empty_cart.png";
+
   /// Assets for imagesLoginPage
   /// assets/images/login_page.png
   static const String imagesLoginPage = "assets/images/login_page.png";
 
+  /// Assets for imagesNoData
+  /// assets/images/no_data.png
+  static const String imagesNoData = "assets/images/no_data.png";
+
   /// Assets for imagesNoInternet
   /// assets/images/no_internet.png
   static const String imagesNoInternet = "assets/images/no_internet.png";
+
+  /// Assets for imagesOrderConfirmed
+  /// assets/images/order_confirmed.png
+  static const String imagesOrderConfirmed = "assets/images/order_confirmed.png";
+
+  /// Assets for imagesOrderDelivered
+  /// assets/images/order_delivered.png
+  static const String imagesOrderDelivered = "assets/images/order_delivered.png";
 
   /// Assets for imagesRegisterPage
   /// assets/images/register_page.png
