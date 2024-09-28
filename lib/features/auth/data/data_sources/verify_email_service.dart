3809793772f125feb3609a5937e833010e3d839d
3oraby <mohaahmed21102003@gmail.com>
2024-09-28 21:03:48 +0300

    solve log out problem

--- lib/features/auth/data/data_sources/verify_email_service.dart
+++ lib/features/auth/data/data_sources/verify_email_service.dart
@@ -1,5 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:e_commerce_app/constants/api_constants.dart';
+import 'package:e_commerce_app/constants/local_constants.dart';
+import 'package:e_commerce_app/core/helpers/functions/extract_refresh_token.dart';
+import 'package:e_commerce_app/core/services/shared_preferences_singleton.dart';
 import 'package:e_commerce_app/features/auth/data/models/verify_email_response_model.dart';
 import 'package:e_commerce_app/core/helpers/api.dart';
 
@@ -13,6 +16,10 @@ class VerifyEmailService {
         url:
             "${ApiConstants.baseUrl}${ApiConstants.verifyEmailEndPoint}token=$token&email=$email",
       );
+      String refreshToken = extractRefreshToken(response: response);
+      // save the refresh token in shared preferences
+      SharedPreferencesSingleton.setString(
+          LocalConstants.refreshTokenNameInPref, refreshToken);
       return VerifyEmailResponseModel.fromJson(response.data);
     } catch (e) {
       throw Exception(
