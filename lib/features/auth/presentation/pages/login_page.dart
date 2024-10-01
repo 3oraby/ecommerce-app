f4672246f399b9b693f3785ce13de39fba24c7c1
3oraby <mohaahmed21102003@gmail.com>
2024-10-01 19:55:06 +0300

    remove unused state

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -30,7 +30,6 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  bool isAsyncCall = false;
   final LoginRequestModel loginRequestModel = LoginRequestModel();
   late AuthCubit authCubit;
   @override
@@ -49,107 +48,95 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return ModalProgressHUD(
-      inAsyncCall: isAsyncCall,
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: BlocListener<AuthCubit, AuthState>(
-          listener: (context, state) {
-            log(state.toString());
-            if (state is AuthNoNetworkErrorState) {
-              setState(() {
-                isAsyncCall = false;
-              });
-              showErrorWithInternetDialog(context);
-            } else if (state is LoginLoadingState) {
-              setState(() {
-                isAsyncCall = true;
-              });
-            } else if (state is LoginLoadedState) {
-              setState(() {
-                isAsyncCall = false;
-              });
-              BlocProvider.of<UserCubit>(context).setUserModel(state.userModel);
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthNoNetworkErrorState) {
+          showErrorWithInternetDialog(context);
+        } else if (state is LoginLoadedState) {
+          BlocProvider.of<UserCubit>(context).setUserModel(state.userModel);
 
-              customShowModalBottomSheet(
-                context: context,
-                imageName: "assets/animations/orderSuccessfullyDone.json",
-                sheetDescription:
-                    "You've logged in successfully. Great to see you again!",
-                onPressed: () {
-                  HomePageNavigationService.navigateToHome();
-                  Navigator.pushNamedAndRemoveUntil(
-                    context,
-                    HomePage.id,
-                    (Route<dynamic> route) => false,
-                  );
-                },
+          customShowModalBottomSheet(
+            context: context,
+            imageName: "assets/animations/orderSuccessfullyDone.json",
+            sheetDescription:
+                "You've logged in successfully. Great to see you again!",
+            onPressed: () {
+              HomePageNavigationService.navigateToHome();
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                HomePage.id,
+                (Route<dynamic> route) => false,
               );
-            } else if (state is LoginErrorState) {
-              setState(() {
-                isAsyncCall = false;
-              });
-              showCustomSnackBar(context, state.message);
-            }
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Form(
-              key: formKey,
-              child: ListView(
-                reverse: true,
-                children: [
-                  Image.asset(
-                    AppImages.imagesLoginPage,
-                    height: 300,
-                  ),
-                  Text(
-                    "Log in",
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.pollerOne(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
+            },
+          );
+        } else if (state is LoginErrorState) {
+          showCustomSnackBar(context, state.message);
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+          inAsyncCall: (state is LoginLoadingState) ? true : false,
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Form(
+                key: formKey,
+                child: ListView(
+                  reverse: true,
+                  children: [
+                    Image.asset(
+                      AppImages.imagesLoginPage,
+                      height: 300,
                     ),
-                  ),
-                  const VerticalGap(30),
-                  CustomTextFormFieldWidget(
-                    labelText: "Email",
-                    prefixIcon: const Icon(Icons.email),
-                    onChanged: (email) {
-                      loginRequestModel.email = email;
-                    },
-                    validator: Validators.emailValidator,
-                  ),
-                  const VerticalGap(30),
-                  CustomTextFormFieldWidget(
-                    labelText: "Password",
-                    isObscure: true,
-                    prefixIcon: const Icon(Icons.lock),
-                    onChanged: (password) {
-                      loginRequestModel.password = password;
-                    },
-                    validator: Validators.validateLoginPassword,
-                  ),
-                  const VerticalGap(30),
-                  CustomTriggerButton(
-                    onPressed: _handleLogin,
-                    description: "Log In",
-                  ),
-                  AuthSwitchWidget(
-                    promptText: "don't have an account?",
-                    actionText: "Register",
-                    onActionPressed: () {
-                      Navigator.pushReplacementNamed(
-                          context, AddAddressPage.id);
-                    },
-                  ),
-                  const VerticalGap(100),
-                ].reversed.toList(),
+                    Text(
+                      "Log in",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.pollerOne(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const VerticalGap(30),
+                    CustomTextFormFieldWidget(
+                      labelText: "Email",
+                      prefixIcon: const Icon(Icons.email),
+                      onChanged: (email) {
+                        loginRequestModel.email = email;
+                      },
+                      validator: Validators.emailValidator,
+                    ),
+                    const VerticalGap(30),
+                    CustomTextFormFieldWidget(
+                      labelText: "Password",
+                      isObscure: true,
+                      prefixIcon: const Icon(Icons.lock),
+                      onChanged: (password) {
+                        loginRequestModel.password = password;
+                      },
+                      validator: Validators.validateLoginPassword,
+                    ),
+                    const VerticalGap(30),
+                    CustomTriggerButton(
+                      onPressed: _handleLogin,
+                      description: "Log In",
+                    ),
+                    AuthSwitchWidget(
+                      promptText: "don't have an account?",
+                      actionText: "Register",
+                      onActionPressed: () {
+                        Navigator.pushReplacementNamed(
+                            context, AddAddressPage.id);
+                      },
+                    ),
+                    const VerticalGap(100),
+                  ].reversed.toList(),
+                ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
