1bbb68c315db0a3dff5d6b55949b8dbe4b1f16b9
3oraby <mohaahmed21102003@gmail.com>
2024-10-03 06:54:51 +0300

    solve problem when pop from profile page

--- lib/features/home/presentation/widgets/home_app_bar.dart
+++ lib/features/home/presentation/widgets/home_app_bar.dart
@@ -1,69 +1,66 @@
-
 import 'package:e_commerce_app/core/helpers/functions/is_user_signed_in.dart';
 import 'package:e_commerce_app/core/helpers/functions/show_not_signed_in_dialog.dart';
 import 'package:e_commerce_app/core/models/user_model.dart';
 import 'package:e_commerce_app/core/utils/app_assets/images/app_images.dart';
+import 'package:e_commerce_app/core/utils/styles/text_styles.dart';
 import 'package:e_commerce_app/features/user/presentation/cubit/user_cubit.dart';
 import 'package:e_commerce_app/features/user/presentation/pages/edit_user_profile_page.dart';
 import 'package:e_commerce_app/core/widgets/horizontal_gap.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class HomeAppBar {
   static AppBar getHomeAppBar({
     required BuildContext context,
   }) {
-    final UserModel? userModel =
-        BlocProvider.of<UserCubit>(context).getUserModel;
-
-    final String displayName;
-    if (userModel != null) {
-      final nameParts = userModel.userName?.split(' ');
-      displayName =
-          nameParts != null && nameParts.isNotEmpty ? nameParts.first : 'User';
-    } else {
-      displayName = "User";
-    }
     return AppBar(
       elevation: 0,
       backgroundColor: Colors.white,
       surfaceTintColor: Colors.transparent,
-      title: Row(
-        children: [
-          GestureDetector(
-            onTap: () {
-              isUserSignedIn()
-                  ? Navigator.pushNamed(context, EditUserProfilePage.id)
-                  : showNotSignedInDialog(context);
-            },
-            child: Image.asset(
-              AppImages.imagesUserPhoto,
-              width: 50,
-              height: 50,
-            ),
-          ),
-          const HorizontalGap(24),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      title: BlocBuilder<UserCubit, UserState>(
+        builder: (context, state) {
+          final UserModel? userModel = context.read<UserCubit>().getUserModel;
+
+          final String displayName =
+              userModel != null ? userModel.userName!.split(' ').first : "User";
+
+          return Row(
             children: [
-              Text(
-                "Hi, $displayName",
-                style: GoogleFonts.aDLaMDisplay(
-                  color: Colors.black,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+              GestureDetector(
+                onTap: () {
+                  isUserSignedIn()
+                      ? Navigator.pushNamed(context, EditUserProfilePage.id)
+                      : showNotSignedInDialog(context);
+                },
+                child: Image.asset(
+                  AppImages.imagesUserPhoto,
+                  width: 50,
+                  height: 50,
                 ),
               ),
-              const Text(
-                "let's go shopping",
-                style: TextStyle(
-                  fontSize: 18,
+              const HorizontalGap(24),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Hi, $displayName",
+                      style: TextStyles.aDLaMDisplayBlackBold24,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const Text(
+                      "let's go shopping",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
-          ),
-        ],
+          );
+        },
       ),
     );
   }
